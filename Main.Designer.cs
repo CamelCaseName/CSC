7e198d02df5ca3cd91adb3b4e8465480b95c9bd8
Leonhard Seidel <leonhardseidel@gmx.de>
2025-09-08 11:28:10 +0200

    node dragging and line drawing added

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,12 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            Menu = new ToolStrip();
+            StartButton = new ToolStripButton();
+            Add = new ToolStripButton();
+            Details = new PropertyGrid();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            Menu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Menu
+            // 
+            Menu.Items.AddRange(new ToolStripItem[] { StartButton, Add });
+            Menu.Location = new Point(0, 0);
+            Menu.Name = "Menu";
+            Menu.Size = new Size(1327, 25);
+            Menu.TabIndex = 0;
+            Menu.Text = "toolStrip1";
+            // 
+            // StartButton
+            // 
+            StartButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            StartButton.Image = (Image)resources.GetObject("StartButton.Image");
+            StartButton.ImageTransparentColor = Color.Magenta;
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(35, 22);
+            StartButton.Text = "Start";
+            StartButton.Click += Start_Click;
+            // 
+            // Add
+            // 
+            Add.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            Add.Image = (Image)resources.GetObject("Add.Image");
+            Add.ImageTransparentColor = Color.Magenta;
+            Add.Name = "Add";
+            Add.Size = new Size(33, 22);
+            Add.Text = "Add";
+            Add.Click += Add_Click;
+            // 
+            // Details
+            // 
+            Details.AllowDrop = true;
+            Details.Anchor = AnchorStyles.None;
+            Details.HelpVisible = false;
+            Details.Location = new Point(864, 54);
+            Details.MaximumSize = new Size(600, 900);
+            Details.MinimumSize = new Size(100, 50);
+            Details.Name = "Details";
+            Details.Size = new Size(414, 438);
+            Details.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(245, 169);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.MouseClick += Button1Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(395, 233);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Button2Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(486, 114);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(449, 401);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(613, 232);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(1327, 623);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(Details);
+            Controls.Add(Menu);
+            MinimumSize = new Size(400, 200);
+            Name = "Main";
+            ShowIcon = false;
+            Text = "Form1";
+            Click += Main_Click;
+            Paint += Main_Paint;
+            DoubleClick += Main_DoubleClick;
+            MouseMove += Main_MouseMove;
+            Menu.ResumeLayout(false);
+            Menu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ToolStrip Menu;
+        private ToolStripButton StartButton;
+        private ToolStripButton Add;
+        private PropertyGrid Details;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
--- Main.cs
+++ Main.cs
@@ -1,10 +1,137 @@
+using Microsoft.VisualBasic.Devices;
+using System.Drawing.Drawing2D;
+using Translator.Explorer.JSONItems;
+
 namespace CSC
 {
     public partial class Main : Form
     {
+        public bool MovingChild = false;
+        Size OffsetFromDragClick = Size.Empty;
+        private Control? movedChild;
+        Point start;
+        Point end;
+        Pen linePen;
+
         public Main()
         {
             InitializeComponent();
+            button1.MouseMove += Main_MouseMove;
+            button2.MouseMove += Main_MouseMove;
+            linePen = new Pen(Brushes.Black, 2);
+            linePen.EndCap = LineCap.Triangle;
+            linePen.StartCap = LineCap.Round;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Click(object sender, EventArgs e)
+        {
+            var pos = PointToClient(Cursor.Position);
+            movedChild = GetChildAtPoint(pos + new Size(4, 4));
+            movedChild ??= GetChildAtPoint(pos + new Size(4, -4));
+            movedChild ??= GetChildAtPoint(pos + new Size(-4, 4));
+            movedChild ??= GetChildAtPoint(pos + new Size(-4, -4));
+            if (movedChild is not null)
+            {
+                if (movedChild.GetType() == typeof(ToolStrip))
+                {
+                    return;
+                }
+
+                MovingChild = !MovingChild;
+                if (MovingChild)
+                {
+                    OffsetFromDragClick = new Size(movedChild.Location.X - pos.X, movedChild.Location.Y - pos.Y);
+                }
+            }
+        }
+
+        private void Main_DoubleClick(object sender, EventArgs e)
+        {
+            Details.Visible = !Details.Visible;
+        }
+
+        private void Main_MouseMove(object? sender, MouseEventArgs e)
+        {
+            if (MovingChild && movedChild is not null)
+            {
+                movedChild.Location = e.Location + OffsetFromDragClick;
+                Invalidate();
+
+            }
+        }
+
+        //draw bezier curves from one button to other
+
+        //to smoothly enter and exit we must set the y axis to the same value as the button, and the x value to some value in between both buttons
+
+        private void Button1Click(object sender, MouseEventArgs e)
+        {
+            Details.SelectedObject = new Criterion();
+        }
+
+        private void Button2Click(object sender, EventArgs e)
+        {
+            Details.SelectedObject = new BackgroundChatterResponse();
+        }
+
+        private void Main_Paint(object sender, PaintEventArgs e)
+        {
+            //todo filter, duh
+            DrawEdge(e, button1, button2);
+            DrawEdge(e, button1, button3);
+            DrawEdge(e, button2, button4);
+            DrawEdge(e, button3, button5);
+            DrawEdge(e, button5, button1);
+
+        }
+
+        private void DrawEdge(PaintEventArgs e, Control parent, Control child)
+        {
+            start = parent.Location + new Size(parent.Size.Width, parent.Size.Height / 2);
+            end = child.Location + new Size(0, child.Size.Height / 2);
+
+
+            Point controlStart;
+            Point controlEnd;
+            int controlEndY, controlStartY;
+
+            int distanceX = Math.Abs(end.X - start.X);
+            if (start.X < end.X)
+            {
+                controlStart = new Point((distanceX / 2) + start.X, start.Y);
+                controlEnd = new Point(((end.X - start.X) / 2) + start.X, end.Y);
+            }
+            else
+            {
+                int distanceY = Math.Abs(end.Y - start.Y);
+                if (start.Y > end.Y)
+                {
+                    controlStartY = start.Y - distanceY / 2;
+                    controlEndY = end.Y + distanceY / 2;
+                }
+                else
+                {
+                    controlStartY = start.Y + distanceY / 2;
+                    controlEndY = end.Y - distanceY / 2;
+                }
+                controlStart = new Point((start.X + distanceX / 2), controlStartY);
+                controlEnd = new Point((end.X - distanceX / 2), controlEndY);
+            }
+
+            e.Graphics.DrawBezier(linePen, start, controlStart, controlEnd, end);
+            e.Graphics.DrawEllipse(Pens.Green, new Rectangle(controlStart, new Size(4, 4)));
+            e.Graphics.DrawEllipse(Pens.Red, new Rectangle(controlEnd, new Size(4, 4)));
+            
         }
     }
 }
